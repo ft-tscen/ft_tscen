05f6e7ef266228867ca0f2235a0ad15f3b3788ff
김동현 <dbin67@naver.com>
2023-02-10 16:54:42 +0900

    fix conflicts

--- srcs/frontend/src/example/login.tsx
+++ srcs/frontend/src/example/login.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { api } from "./api";
+import { api } from "../axios/api";
 
 const LoginTest = () => {
 	const [loggedIn, setloggedIn] = useState(false);
