b341429a1648b3bd58e6cc7268a510c33dc9cac3
skdddfhg123 <skdddfhg6@gmail.com>
2023-03-04 13:23:45 +0900

    feat: avatar 공간 추가

--- srcs/frontend/src/components/game/PlayerBar.tsx
+++ srcs/frontend/src/components/game/PlayerBar.tsx
@@ -41,24 +41,29 @@ function Player({ mod }: gameComponent) {
 	return (
 		<>
 		{lplayer && rplayer && (
-		<div style={{ display: "flex", height: "100vh", color: "white" }}>
-			<div style={{ display: "flex" }}>
-				{/* <div style={{ width: "120px", height: "120px", overflow: "hidden", borderRadius: "50%", marginRight: "20px" }}>
-					<img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style={{ width: "100%" }} />
-				</div> */}
-				<div>
-					<h2 style={{ fontSize: '2rem', margin: 0 }}>{lplayer.name}</h2>
-					<p style={{ fontSize: '1.5rem', margin: 0 }}>Wins: {lplayer.wins}</p>
-					<p style={{ fontSize: '1.5rem', margin: 0 }}>Losses: {lplayer.losses}</p>
-				</div>
-			</div>
-			<div style={{ flex: "1", display: "flex", flexDirection: "column", alignItems: "flex-end" }}>
-				<div style={{ textAlign: "right" }}>
-					<h2 style={{ fontSize: '2rem', margin: 0 }}>{rplayer.name}</h2>
-					<p style={{ fontSize: '1.5rem', margin: 0 }}>Wins: {rplayer.wins}</p>
-					<p style={{ fontSize: '1.5rem', margin: 0 }}>Losses: {rplayer.losses}</p>
-				</div>
-			</div>
+		<div style={{ display: "flex", height: "120px", color: "white" }}>
+		<div style={{ display: "flex", flex: "1" }}>
+		<div style={{ width: "120px", height: "120px", overflow: "hidden", borderRadius: "50%", marginRight: "20px" }}>
+		<img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style={{ width: "100%" }} />
+		</div>
+		<div>
+		<h2 style={{ fontSize: '2rem', margin: 0 }}>{lplayer.name}</h2>
+		<p style={{ fontSize: '1.5rem', margin: 0 }}>Wins: {lplayer.wins}</p>
+		<p style={{ fontSize: '1.5rem', margin: 0 }}>Losses: {lplayer.losses}</p>
+		</div>
+		</div>
+		<div style={{ width: "120px", display: "flex", alignItems: "flex-end" }}>
+		</div>
+		<div style={{ flex: "1", display: "flex", flexDirection: "column", alignItems: "flex-end" }}>
+		<div style={{ textAlign: "right", paddingRight: "20px" }}>
+		<h2 style={{ fontSize: '2rem', margin: 0 }}>{rplayer.name}</h2>
+		<p style={{ fontSize: '1.5rem', margin: 0 }}>Wins: {rplayer.wins}</p>
+		<p style={{ fontSize: '1.5rem', margin: 0 }}>Losses: {rplayer.losses}</p>
+		</div>
+		</div>
+		<div style={{ width: "120px", height: "120px", overflow: "hidden", borderRadius: "50%" }}>
+		<img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style={{ width: "100%" }} />
+		</div>
 		</div>
 		)}
 		</>
